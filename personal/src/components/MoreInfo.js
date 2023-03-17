2838fa39d4e68ad5a2df47678659e9a79c4979f3
jesusoyer <oyervidesjesus017@gmail.com>
2023-03-17 16:04:30 -0500

    added project images and added resume to more info page

--- personal/src/components/MoreInfo.js
+++ personal/src/components/MoreInfo.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import './app.css';
 import headShot from './assets/images/FullSizeRender (3).jpeg'
+import resumeImage from './assets/images/resumeScreenShot.png'
 class MoreInfo extends React.Component{
+  
+   
+
     render(){
+      const onButtonClick= () =>{
+        fetch('resume draft 5.0.pdf').then(response => {
+         response.blob().then(blob=> {
+          const fileURL = window.URL.createObjectURL(blob);
+          let alink = document.createElement('a');
+          alink.href = fileURL
+          alink.download = 'resume draft 50.pdf';
+          alink.click();
+         })
+      })
+    } 
       return(
         
         <div className='contactContainer'> 
-         <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, a nisi. Natus similique alias laboriosam illum in delectus eveniet ab totam aspernatur omnis nam, quod magni nostrum ipsa ex quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, beatae illo nisi sit aspernatur modi. Error necessitatibus sed enim maxime. Dicta qui ratione perspiciatis quidem minus, consectetur placeat veritatis nemo! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat quasi incidunt deleniti unde labore omnis minus delectus in itaque, quam maiores nulla, odio, quod voluptas praesentium suscipit quisquam atque perferendis!
+
+         <div className='resumeCard'>
+         <img className='resumeImage' src={resumeImage} alt='resume'></img>
+         <button className="resumeButton" onClick={onButtonClick}>Download Resume...</button>
+         
   </div>
 
   <div className="moreInfoCard">
   <img className='moreInfoImage' src={headShot} alt='Jesus'></img>
-      
+  
       <div className="socialMediaGrid">
        <div> Location: Austin, TX</div>
-     <div><a  href = "mailto: oyervidesjesus017@gmail.com" rel='noopener noreferrer' target='_blank'>Email: oyervidesjesus017@gmail.com</a>
+     <div><a className='mediaLinks' href = "mailto: oyervidesjesus017@gmail.com" rel='noopener noreferrer' target='_blank'>Email: oyervidesjesus017@gmail.com</a>
      </div> 
 
-      <div><a  href = "https://www.linkedin.com/in/jesus-oyervides-jr/" rel='noopener noreferrer' target='_blank'>LinkedIn: https://www.linkedin.com/in/jesus-oyervides-jr/</a></div>
+      <div><a className='mediaLinks' href = "https://www.linkedin.com/in/jesus-oyervides-jr/" rel='noopener noreferrer' target='_blank'>LinkedIn: https://www.linkedin.com/in/jesus-oyervides-jr/</a></div>
 
-      <div><a  href = "https://github.com/jesusoyer" rel='noopener noreferrer' target='_blank'>GitHub: https://github.com/jesusoyer</a> </div>
+      <div><a  className='mediaLinks' href = "https://github.com/jesusoyer" rel='noopener noreferrer' target='_blank'>GitHub: https://github.com/jesusoyer</a> </div>
+      </div>
+      
+      </div>
+
+        
 
       </div>
-      </div>
-        </div>
+        
       )
     }
   }
--- personal/src/components/Projects.js
+++ personal/src/components/Projects.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './app.css';
-import meInNature from './assets/images/arkansasPic.jpeg'
+import gymRatsImage from './assets/images/gymRatsImage.png'
+import eagleEye from './assets/images/eagleeye.png'
+import videoWebsite from './assets/images/Video_app_image.png'
+import imageWebsite from './assets/images/searchImageApp.png'
+import dailyPlanner from './assets/images/DayPlanner.png'
+import weatherDashboard from './assets/images/weatherDashboard.png'
+import bibliotecha from './assets/images/biblioTecha.png'
 
 class Projects extends React.Component{
     render(){
@@ -9,21 +15,35 @@ class Projects extends React.Component{
         <div className="projectsGrid">
     <div className="projectHeader">Projects</div>
 
-    <div className="featuredProject"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maiores explicabo, dolore eligendi facere debitis aliquid, harum accusamus quis obcaecati excepturi provident ullam ea placeat repudiandae quo ipsam ratione rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, perferendis. Quo rerum ad excepturi quae, sed asperiores magnam! Fuga eaque, debitis dolores aut totam delectus praesentium obcaecati recusandae. Laudantium, odit!</div>
+    <div className="featuredProject">
+      <div className='projectTitles'>GymRatz</div>
+      <img className='featuredImage' src={gymRatsImage} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique maiores explicabo, dolore eligendi facere debitis aliquid, harum accusamus quis obcaecati excepturi provident ullam ea placeat repudiandae quo ipsam ratione rem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, perferendis. Quo rerum ad excepturi quae, sed asperiores magnam! Fuga eaque, debitis dolores aut totam delectus praesentium obcaecati recusandae. Laudantium, odit!</div>
 
-    <div className="projectColumns"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores numquam saepe consequatur ab deleniti ad ipsum corporis, architecto repellat maiores ut, libero doloribus obcaecati laborum cupiditate in molestiae! Quaerat.</div>
+    <div className="projectColumns">
+      <div className='projectTitles'>Eagle Eye</div>
+      <img className='featuredImage' src={eagleEye} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius asperiores numquam saepe consequatur ab deleniti ad ipsum corporis, architecto repellat maiores ut, libero doloribus obcaecati laborum cupiditate in molestiae! Quaerat.</div>
 
-    <div className="projectColumns"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam ipsa consequuntur impedit! Aspernatur repudiandae magni nihil quaerat fugit, est delectus et facilis, fuga quod voluptatem sit! Eligendi nulla veritatis culpa?</div>
+    <div className="projectColumns">
+    <div className='projectTitles'>Video Website</div>
+      <img className='featuredImage' src={videoWebsite} alt='nature'></img>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam ipsa consequuntur impedit! Aspernatur repudiandae magni nihil quaerat fugit, est delectus et facilis, fuga quod voluptatem sit! Eligendi nulla veritatis culpa?</div>
 
-    <div className="projectColumns"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, soluta assumenda? Quos vero beatae nostrum nobis impedit. Explicabo, quod accusantium? Sit dolore commodi doloribus tempore iusto fuga esse soluta quae.</div>
+    <div className="projectColumns">
+    <div className='projectTitles'>Image Website</div>
+      <img className='featuredImage' src={imageWebsite} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, soluta assumenda? Quos vero beatae nostrum nobis impedit. Explicabo, quod accusantium? Sit dolore commodi doloribus tempore iusto fuga esse soluta quae.</div>
 
-    <div className="projectColumns"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquam voluptatibus ipsam! Doloremque quidem animi tenetur similique. Perspiciatis rem, accusamus, amet ab aliquid, iste placeat qui ea ut nulla error?</div>
+    <div className="projectColumns">
+    <div className='projectTitles'>Daily Planner</div>
+      <img className='featuredImage' src={dailyPlanner} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aliquam voluptatibus ipsam! Doloremque quidem animi tenetur similique. Perspiciatis rem, accusamus, amet ab aliquid, iste placeat qui ea ut nulla error?</div>
 
-    <div className="projectColumns"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, nostrum mollitia. Perferendis itaque accusantium deserunt sunt architecto molestiae esse, nobis cupiditate eveniet ex sint, adipisci aliquam necessitatibus. Doloremque, quasi neque?</div>
+    <div className="projectColumns">
+    <div className='projectTitles'>Weather Dashboard</div>
+      <img className='featuredImage' src={weatherDashboard} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, nostrum mollitia. Perferendis itaque accusantium deserunt sunt architecto molestiae esse, nobis cupiditate eveniet ex sint, adipisci aliquam necessitatibus. Doloremque, quasi neque?</div>
 
-    <div className="projectColumns"><img className='featuredImage' src={meInNature} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, hic, ipsum assumenda earum harum atque est maxime vel, fugit suscipit illum. Accusamus ex unde maxime! Ducimus aut iste esse rem!</div>
+    <div className="projectColumns">
+    <div className='projectTitles'>BiblioTecha</div>
+      <img className='featuredImage' src={bibliotecha} alt='nature'></img>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, hic, ipsum assumenda earum harum atque est maxime vel, fugit suscipit illum. Accusamus ex unde maxime! Ducimus aut iste esse rem!</div>
 
-    <div className="moreProjects"><a href = "https://github.com/jesusoyer" rel='noopener noreferrer' target='_blank'>Click for more Projects...</a></div>
+    <div className="moreProjects"><a className='linkToGithub' href = "https://github.com/jesusoyer" rel='noopener noreferrer' target='_blank'>Click for more Projects...</a></div>
     
 </div>
       )
